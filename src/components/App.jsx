e1497d268d6f23be628ab8050b343f4474b50a29
Hammad Ahmad <hammadahmad9999@hotmail.com>
2023-06-24 18:06:00 +0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./Login";
+
+var isLogedIn = false;
+
+function App() {
+  return (
+    <div className="container">
+      {isLogedIn ? <h1>Hello</h1> : <Login />}
+    </div>
+  );
+}
+
+export default App;
